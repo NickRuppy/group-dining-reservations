9086ef2b3466562ecd9b04d59da64988e8045699
nickstart <58527559+nickstart@users.noreply.github.com>
2025-01-26 16:17:06 +0100

    fix: add webpack config for undici package transpilation

--- next.config.js
+++ next.config.js
@@ -13,6 +13,21 @@ const nextConfig = {
     // Don't fail build on ESLint errors during deployment
     ignoreDuringBuilds: true,
   },
+  webpack: (config, { isServer }) => {
+    // Add transpilation for undici
+    config.module.rules.push({
+      test: /\.js$/,
+      include: /node_modules\/undici/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-transform-private-methods', '@babel/plugin-transform-class-properties']
+        }
+      }
+    });
+    return config;
+  }
 }
 
 module.exports = nextConfig 
